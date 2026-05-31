08ba17cc532fcc6a54567db55718690abda849d3
chienan0617 <chienan0617@gmail.com>
2026-05-31 23:38:03 +0800

    add

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4272823110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2930650204" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
