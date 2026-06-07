1fffee6a652ef190d67e1e929950f0787eb72d77
chienan0617 <chienan0617@users.noreply.github.com>
2026-06-07 07:04:23 +0000

    Deploy Flutter Web via GitHub Actions chienan0617/Simple-Note@f4a44b77b2ae7e5097de1694f6cd04359c288df2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4172481580" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "666462623" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
